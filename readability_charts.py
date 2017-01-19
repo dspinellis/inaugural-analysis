2fb0e67ea149a02c5abc541c568c320bc267a1f0
Diomidis Spinellis <dds@aueb.gr>
2017-01-19 18:33:38 +0200

    Add more metrics

--- readability_charts.py
+++ readability_charts.py
@@ -7,6 +7,13 @@ import seaborn as sns
 import sys
 from textstat.textstat import textstat
 
+metrics = [
+    'SMOG index',
+    'Flesh reading ease',
+    'Funning fog index',
+    'Number of difficult words',
+]
+
 records = []
 for file_name in sys.argv[1:]:
     with open(file_name, 'r') as myfile:
@@ -21,21 +28,27 @@ for file_name in sys.argv[1:]:
             (year,
              str(first_name + middle_initial + ' ' + last_name + "\n" + year),
              full_name,
-             textstat.smog_index(data)),
-        )
+             textstat.smog_index(data),
+             textstat.flesch_reading_ease(data),
+             textstat.gunning_fog(data),
+             textstat.difficult_words(data)
+        ))
 
 data = np.zeros((len(records),), dtype=[('year', 'i4'),
                                         ('label', 'U20'),
-                                        ('full_name', 'U30'),
-                                        ('SMOG','f4')])
-
+                                        ('full_name', 'U30')] + [
+                                            (x, 'f4') for x in metrics])
 data[:] = records
 df = pd.DataFrame(data)
 df = df.sort_values(by=['year'])
+print(records)
 plt.xticks(rotation=45)
 plt.gcf().subplots_adjust(bottom=0.15)
 plt.gcf().set_size_inches(10, 5)
-g = sns.barplot(x="label", y="SMOG", data=df, color='green')
-g.set_xticklabels(g.get_xticklabels(), rotation=30)
-g.set(ylabel='SMOG', xlabel='Inaugural Address')
-g.get_figure().savefig("SMOG.png", dpi=150)
+
+for plot in metrics:
+    g = sns.barplot(x="label", y=plot, data=df, color='green')
+    g.set_xticklabels(g.get_xticklabels(), rotation=30)
+    g.set(ylabel=plot, xlabel='Inaugural Address')
+    file_name = plot.replace(' ', '_') + '.png'
+    g.get_figure().savefig(file_name, dpi=150)
